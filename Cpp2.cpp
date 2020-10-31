611c12b395457e036b4ee35089e7693c93e5a8d2
arketl <2546581821@qq.com>
2020-11-01 00:29:37 +0800

    first commit

---
+++ Cpp2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	while(scanf("%d%d",&a,&b)!=EOF)
+	{
+		if(a==0&&b==0)
+		{
+			break;
+		}
+		else
+		{
+			sum=a+b;
+			printf("%d\n",sum);
+		}
+	}
+	return 0;
+}
